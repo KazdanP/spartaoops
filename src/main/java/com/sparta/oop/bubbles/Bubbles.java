45332bbdd08a4a5ed89ac2585937182f14196556
KazdanP <Luke.kidd@Hotmail.co.uk>
2022-05-12 11:40:46 +0100

    Added counter and loop breakout

--- src/main/java/com/sparta/oop/bubbles/Bubbles.java
+++ src/main/java/com/sparta/oop/bubbles/Bubbles.java
@@ -4,9 +4,13 @@ public class Bubbles {
 
     public static int[] bubbleBubbles(int[] toSort) {
 
-        int counter = 1;
+        int runCounter = 1;
+        boolean swapped;
 
         for (int j = 0; j <= toSort.length; j++) {
+
+            swapped = false;
+
             for (int i = 0; i < toSort.length - 1 - j; i++) {
                 int first = toSort[i];
                 int second = toSort[i + 1];
@@ -14,12 +18,17 @@ public class Bubbles {
                 if (first > second) {
                     toSort[i]  = second;
                     toSort[i + 1] = first;
+                    swapped = true;
                 }
-                counter++;
+
+                runCounter++;
             }
+
+            //Checks if nothing has been swapped i.e. already in the correct order
+            if (!swapped) { break; }
         }
 
-        System.out.println("Times ran: " + counter);
+        System.out.println("Times ran: " + runCounter);
 
         return toSort;
     }
